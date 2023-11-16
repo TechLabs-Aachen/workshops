9bfda1fcd54280e996381ebb03e467dc731b2b57
v4nderstruck <zensayyy@gmail.com>
2023-11-16 18:15:44 +0100

    TLGrad ignore datasets

---
+++ tutorials/TLGrad_solution/nn/linear.py
@@ -0,0 +1,17 @@
+import numpy.random as npr
+
+def init_parameters(layers, random_state=npr.RandomState(0), scale=0.1):
+    # TODO: layers is a description of the MLP e.g. [768, 250, 10 ]
+    # TODO: return a list of random initialized [A, b] 
+    # TODO: e.g. [ [(768, 250), (1)], [(250, 10), (1)] ] denoting dimensions here
+    # TODO: use random_state.randn(input_dim, output_dim) to generate a random matrix of dims
+    # TODO: multiply scale with every random generated matrix/vector
+    parameters = []
+
+    for i in range(len(layers)-1):
+        A = scale * random_state.randn(layers[i], layers[i+1])
+        b = scale * random_state.randn(layers[i+1])
+        parameters.append([A,b])
+
+    return parameters
+
